6f6dc52ca5abc78073cb3aace8ddbe61cc566b19
Sam Schickler <gadgetsam@gmail.com>
2018-08-01 11:34:03 -0700

    started on sliceing

--- readTiff.py
+++ readTiff.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import os
+import numpy as np
 import shutil
+from scipy import misc, ndimage, io
 import ntpath
 ntpath.basename("a/b/c")
 import json
@@ -11,6 +13,7 @@ def readTiff(filepath):
         pass
     filename = "test"
     im = Image.open(filepath)
+
     try:
 
         os.mkdir("static/data/"+filename)
@@ -19,15 +22,34 @@ def readTiff(filepath):
     infoFile = {"numImages":im.n_frames, "height":im.height, "width":im.width}
     with open("static/test.js", 'w') as outfile:
         outfile.write("var configuration ="+str(json.dumps(infoFile)))
+    h,w = im.width, im.height
+    tiffarray = np.zeros((w, h, im.n_frames))
+
     try:
-        while False:
+        for i in range(im.n_frames):
             im.seek(im.tell()+1)
-            im.save("static/data/"+filename+"/"+str(im.tell())+".png")
+            image1 = misc.fromimage(im, flatten=1)
+            tiffarray[:, :, i] = np.array(image1)
+            # misc.imsave("static/data/"+filename+"/"+str(im.tell())+".png", image1)
     except EOFError:
         pass # end of sequence
+    dim = tiffarray.shape
+    for x in range(3):
+        os.mkdir("static/data/" + filename + "/" + str(x))
+        for i in range(1,dim[x]+1):
+
+            misc.imsave("static/data/"+filename+"/"+str(x)+"/"+str(i)+".png", simple_slice(tiffarray, i-1, x))
+
+    # print(tiffarray)
     return filename
 def path_leaf(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
+def simple_slice(arr, inds, axis):
+    # this does the same as np.take() except only supports simple slicing, not
+    # advanced indexing, and thus is much faster
+    sl = [slice(None)] * arr.ndim
+    sl[axis] = inds
+    return arr[sl]
+readTiff("butterfly_wing_small.tif")
 
-# readTiff("alkaseltzer1_combined_bin2.tif")
